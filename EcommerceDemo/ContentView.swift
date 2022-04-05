1f22c9a63b5aaac5b1b28ffbc01579cec3e58407
Ahmet Özberk <ahmet.ozberk.mrs@gmail.com>
2022-04-05 14:22:16 +0300

    Initial Commit

---
+++ EcommerceDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EcommerceDemo
+//
+//  Created by Ahmet OZBERK on 5.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EcommerceDemo/EcommerceDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceDemoApp.swift
+//  EcommerceDemo
+//
+//  Created by Ahmet OZBERK on 5.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
